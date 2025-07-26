45a2fd04bf6ea7ad2d1e13c16451dadd44337c2e
qibal <iqbalherlambang.39@gmail.com>
2025-07-26 23:03:18 +0700

    update swagger

--- docs/docs.go
+++ docs/docs.go
@@ -3572,7 +3572,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "my-manajer-production.up.railway.app",
 	BasePath:         "/api/v1",
 	Schemes:          []string{"http", "https"},
 	Title:            "My Manajer API",
